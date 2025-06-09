cb325a4e2b434ef326b29a962f4d2327cb81ce96
YunSwain <destap@naver.com>
2025-06-09 20:19:45 +0900

    [Feat]: First Commit

---
+++ RecipeForT/RecipeForTApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeForTApp.swift
+//  RecipeForT
+//
+//  Created by Swain Yun on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeForTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
