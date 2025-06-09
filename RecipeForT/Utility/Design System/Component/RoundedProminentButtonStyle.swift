73331e9280934ae21cd6891c17ffdd8adcfdda51
YunSwain <destap@naver.com>
2025-06-09 20:20:00 +0900

    [Feat]: 둥근 모서리 버튼 스타일 정의

---
+++ RecipeForT/Utility/Design System/Component/RoundedProminentButtonStyle.swift
@@ -0,0 +1,56 @@
+//
+//  RoundedProminentButtonStyle.swift
+//  RecipeForT
+//
+//  Created by Swain Yun on 6/8/25.
+//
+
+import SwiftUI
+
+struct RoundedProminentButtonStyle: ButtonStyle {
+    let disabled: Bool
+    let foregroundColor: Color
+    let backgroundColor: Color
+    let isLoading: Bool
+    
+    init(
+        _ disabled: Bool,
+        _ foregroundColor: Color,
+        _ backgroundColor: Color,
+        _ isLoading: Bool
+    ) {
+        self.foregroundColor = foregroundColor
+        self.backgroundColor = backgroundColor
+        self.disabled = disabled || isLoading
+        self.isLoading = isLoading
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            if isLoading {
+                ProgressView()
+                    .tint(foregroundColor)
+            } else {
+                configuration.label
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .foregroundStyle(foregroundColor)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(disabled ? .gray : backgroundColor)
+        )
+    }
+}
+
+extension ButtonStyle where Self == RoundedProminentButtonStyle {
+    static func roundedProminent(
+        disabled: Bool = false,
+        foreground: Color = .white,
+        background: Color = .gray,
+        isLoading: Bool = false
+    ) -> RoundedProminentButtonStyle {
+        RoundedProminentButtonStyle(disabled, foreground, background, isLoading)
+    }
+}
+
