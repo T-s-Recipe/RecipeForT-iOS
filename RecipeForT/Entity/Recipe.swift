25e58537da08697c8f53d707b227cf64086b2af5
YunSwain <destap@naver.com>
2025-06-11 00:12:09 +0900

    [Refactor]: 레시피 도메인 모델 일부 프로퍼티 추가

--- RecipeForT/Entity/Recipe.swift
+++ RecipeForT/Entity/Recipe.swift
@@ -23,4 +23,10 @@ struct Recipe: Identifiable {
     let cookingTime: TimeInterval
     /// 레시피 등재자 이름
     let ownerName: String
+    /// 기타 레시피에 대한 설명 등의 부가정보
+    let additionalInformation: String
+    
+    static func mock() -> Self {
+        .init(id: 0, title: "브로콜리 두부 무침", imageURL: nil, servingsCount: 3, cost: 4000, cookingTime: 15, ownerName: "홍길동", additionalInformation: "기타 메모는 여기에. 참고한 레시피 원본 출저 등의 내용 적으면 됨")
+    }
 }
