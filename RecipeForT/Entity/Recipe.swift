4cdbebf4d4d8142c35c829ff1417a01ef989ff40
YunSwain <destap@naver.com>
2025-06-11 23:35:26 +0900

    [Refactor]: Recipe 도메인 모델 개선 중

--- RecipeForT/Entity/Recipe.swift
+++ RecipeForT/Entity/Recipe.swift
@@ -9,24 +9,46 @@ import Foundation
 
 /// 레시피
 struct Recipe: Identifiable {
-    /// 고유식별자
+    /// 고유 식별자
     let id: UInt64
+    /// 레시피 작성자 식별자
+    let authorID: UInt64
     /// 레시피 또는 음식의 이름
-    let title: String
+    let name: String
     /// 대표 이미지 URL
     let imageURL: URL?
     /// n인분, 인당 식사 분량
     let servingsCount: UInt8
     /// 조리비용
     let cost: Decimal
-    /// 조리시간
+    /// 조리시간(분)
     let cookingTime: TimeInterval
-    /// 레시피 등재자 이름
-    let ownerName: String
-    /// 기타 레시피에 대한 설명 등의 부가정보
-    let additionalInformation: String
-    
-    static func mock() -> Self {
-        .init(id: 0, title: "브로콜리 두부 무침", imageURL: nil, servingsCount: 3, cost: 4000, cookingTime: 15, ownerName: "홍길동", additionalInformation: "기타 메모는 여기에. 참고한 레시피 원본 출저 등의 내용 적으면 됨")
-    }
+    /// 레시피에 대한 설명
+    let description: String
+    /// 재료 목록
+    let ingredients: [Ingredient]
+}
+
+/// 음식 재료
+struct Ingredient {
+    /// 재료 이름
+    let name: String
+    /// 계량 단위
+    let unit: IngredientUnit
+}
+
+/// 계량 단위
+enum IngredientUnit {
+    case count(Double)
+    case gram(Double)
+    case milliliter(Double)
+    case tablespoon(Double)
+    case teaspoon(Double)
+}
+
+/// 조리과정 단계적 구분
+enum CookingStep {
+    // 재료손질
+    // 양념만들기
+    // 조리
 }
