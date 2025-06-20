3b99c9c9f9212ec929b53a29b1a534c11b81fe9e
YunSwain <destap@naver.com>
2025-06-20 22:45:09 +0900

    [Feat]: LoginView 와이어프레임 구현

--- RecipeForT/Feature/Login/View/LoginView.swift
+++ RecipeForT/Feature/Login/View/LoginView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AuthenticationServices
 
 struct LoginView: View {
     @EnvironmentObject private var router: Router
@@ -26,39 +27,99 @@ struct LoginView: View {
             
             Spacer()
             
-            VStack(spacing: 24) {
-                Image(.logo)
-                    .padding(.bottom, 12)
+            SignInView()
+            
+            Spacer()
+        }
+    }
+}
+
+// MARK: - Subviews
+struct LoginSection: View {
+    var body: some View {
+        VStack(spacing: 24) {
+            Image(.logo)
+                .padding(.bottom, 12)
+            
+            Button {
+                // TODO: SignInView로 연결
+            } label: {
+                Text("Log in")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                    .background(.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+            }
+            .tint(.white)
+            
+            Button {
+                // TODO: SignUpView로 연결
+            } label: {
+                Text("Sign up for free")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.white)
+                            .strokeBorder(.gray)
+                    )
+            }
+            .tint(.black)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct SignInView: View {
+    @State private var emailTextField: String = String()
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Text("Welcome back")
+                .font(.title.weight(.medium))
+                .padding(.bottom, 12)
+            
+            SignInWithAppleButton(.continue) { request in
                 
-                Button {
-                    // TODO: SignInView로 연결
-                } label: {
-                    Text("Log in")
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical)
-                        .background(.black)
-                        .clipShape(RoundedRectangle(cornerRadius: 5))
-                }
-                .tint(.white)
+            } onCompletion: { result in
+                
+            }
+            .signInWithAppleButtonStyle(.whiteOutline)
+            .frame(height: 44)
+
+            Text("OR")
+            
+            HStack {
+                Text("Email address")
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+            }
+            
+            RoundedTextField("(e.g) tsrecipe@gmail.com", text: $emailTextField)
+            
+            Button {
+                
+            } label: {
+                Text("Continue")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical)
+                    .background(.gray)
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+            }
+            .tint(.white)
+            
+            HStack {
+                Text("Don't have an account?")
                 
                 Button {
                     // TODO: SignUpView로 연결
                 } label: {
-                    Text("Sign up for free")
-                        .frame(maxWidth: .infinity)
-                        .padding(.vertical)
-                        .background(
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(.white)
-                                .strokeBorder(.gray)
-                        )
+                    Text("Sign up")
                 }
-                .tint(.black)
             }
-            .padding(.horizontal)
-            
-            Spacer()
         }
+        .padding(.horizontal)
     }
 }
 
