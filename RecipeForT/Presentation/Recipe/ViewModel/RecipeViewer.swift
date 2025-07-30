116d66cad74a439b23130a25c2f135ead685730d
YunSwain <destap@naver.com>
2025-07-31 00:11:01 +0900

    [Feat]: 레시피 가이드 화면 뷰모델 역할의 RecipeViewer 선언 (WIP)

---
+++ RecipeForT/Presentation/Recipe/ViewModel/RecipeViewer.swift
@@ -0,0 +1,68 @@
+//
+//  RecipeViewer.swift
+//  RecipeForT
+//
+//  Created by Swain Yun on 7/29/25.
+//
+
+import Foundation
+
+@MainActor @Observable
+final class RecipeViewer {
+    private(set) var servings: Decimal?
+    private(set) var ingredients: [Ingredient]
+    private(set) var sources: [Ingredient]
+    private(set) var detailedSteps: [CookingStep]
+    
+    private let maxServing: Decimal = 100
+    private let minServing: Decimal = 1
+    private let originalServings: Decimal
+    private let originalIngredients: [Ingredient]
+    private let originalSources: [Ingredient]
+    
+    init(_ recipe: Recipe) {
+        self.servings = recipe.servingsCount
+        self.ingredients = recipe.ingredients
+        self.sources = recipe.sources
+        self.detailedSteps = recipe.detailedSteps
+        self.originalServings = recipe.servingsCount ?? 1
+        self.originalIngredients = recipe.ingredients
+        self.originalSources = recipe.sources
+    }
+    
+    private func adjustQuantities() {
+        guard let servings = servings, servings >= minServing, servings <= maxServing else { return }
+        let ratio = servings / originalServings
+        
+        ingredients.enumerated().forEach { index, _ in
+            for unit in MeasurementUnit.allCases {
+                if let originalQuantity = originalIngredients[index].units[unit] {
+                    ingredients[index].units[unit] = (originalQuantity * ratio).rounded(to: 2)
+                }
+            }
+        }
+        
+        sources.enumerated().forEach { index, _ in
+            for unit in MeasurementUnit.allCases {
+                if let originalQuantity = originalSources[index].units[unit] {
+                    sources[index].units[unit] = (originalQuantity * ratio).rounded(to: 2)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Interfaces
+extension RecipeViewer {
+    func increaseServing() {
+        guard let currentServings = servings, currentServings < maxServing else { return }
+        servings = (currentServings + 0.5).rounded(to: 1)
+        adjustQuantities()
+    }
+    
+    func decreaseServing() {
+        guard let currentServings = servings, currentServings > minServing else { return }
+        servings = (currentServings - 0.5).rounded(to: 1)
+        adjustQuantities()
+    }
+}
