5e1beeb11967d1f4878fa41d86f98da877f344ff
YunSwain <destap@naver.com>
2025-08-12 02:14:40 +0900

    [Refactor]: View-Feature Architecture로 전환 (WIP)

---
+++ RecipeForT/Features/Preference/PreferenceFeature.swift
@@ -0,0 +1,144 @@
+//
+//  PreferenceFeature.swift
+//  RecipeForT
+//
+//  Created by Swain Yun on 8/12/25.
+//
+
+import SwiftUI
+
+struct PreferenceFeature {
+    enum Constants: String, CustomStringConvertible {
+        case noticeSectionTitle = "Notice"
+        case QNASectionTitle = "Q&A"
+        
+        var description: String { self.rawValue }
+    }
+    @Environment(\.router) private var router
+}
+
+// MARK: - ViewFeature Conformation
+extension PreferenceFeature: ViewFeature {
+    enum UIEvent {
+        
+    }
+    
+    func notify(_ event: UIEvent) {
+        
+    }
+}
+
+// MARK: - View Conformation
+extension PreferenceFeature: View {
+    var body: some View {
+        ScrollView(.vertical) {
+            accountSection
+            
+            thickDivider
+            
+            noticeSection(Constants.noticeSectionTitle.description)
+            
+            thickDivider
+            
+            noticeSection(Constants.QNASectionTitle.description)
+            
+            thickDivider
+            
+            HStack(spacing: 16) {
+                Text("App version")
+                Text("0.0.1")
+            }
+            .padding()
+            
+            HStack(spacing: 12) {
+                Button {
+                    // TODO: 계정 비활성화
+                } label: {
+                    Text("Deactivate Account")
+                }
+                .tint(.gray)
+                
+                Text("|")
+                
+                Button {
+                    // TODO: 회원탈퇴
+                } label: {
+                    Text("Delete Account")
+                }
+                .tint(.gray)
+            }
+            .padding()
+        }
+    }
+    
+    private var thickDivider: some View {
+        Rectangle()
+            .frame(height: 10)
+            .foregroundStyle(.gray.opacity(0.3))
+    }
+    
+    private var accountSection: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("User ID")
+                .font(.title2.bold())
+                .padding(.vertical, 8)
+            
+            Text("Signed up with")
+            
+            HStack {
+                Text("Google")
+                Spacer()
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(.gray.opacity(0.1))
+                    .strokeBorder(.gray)
+            )
+        }
+        .padding()
+    }
+    
+    @ViewBuilder
+    private func noticeSection(_ title: String) -> some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .font(.headline.bold())
+            
+            cell()
+            Divider()
+            cell()
+            Divider()
+            cell()
+            
+            Button {
+                router.route(to: .loginView)
+            } label: {
+                Text("See more")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 5)
+                            .fill(.clear)
+                            .strokeBorder(.gray)
+                    )
+            }
+            .tint(.black)
+        }
+        .padding()
+    }
+    
+    @ViewBuilder private func cell() -> some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text("25.Jun.19")
+                .foregroundStyle(.gray)
+            
+            Text("Lorem ipsum dolor sit amet consectetur. Dictumst pellentesque vivamus quam turpis faucibus in. Posuere sed aliquet et egestas leo lacinia non egestas leo lacinia")
+                .lineLimit(3)
+        }
+    }
+}
+
+#Preview {
+    PreferenceFeature()
+}
